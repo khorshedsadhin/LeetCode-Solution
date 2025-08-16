ecb18c9f06d05a6f66c8e37c84a7334730de4a02
khorshedsadhin <khorshedsadhin81@gmail.com>
2025-08-16 11:31:42 +0600

    fixed python code structure and some core logics through pyright

--- Clone Graph.py
+++ Clone Graph.py
@@ -1,14 +1,19 @@
+from typing import Optional
+
+
 class Node:
-    def __init__(self, val = 0, neighbors = None):
+    def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-from typing import Optional
+
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
         oldToNew = {}
 
-        def dfs(node: Optional['Node']) -> Optional['Node']:
+        def dfs(node: Optional["Node"]) -> Optional["Node"]:
+            if not node:
+                return None
             if node in oldToNew:
                 return oldToNew[node]
 
