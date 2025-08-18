a83507c43333f309c0ef891779892af8265966be
khorshedsadhin <khorshedsadhin81@gmail.com>
2025-08-19 01:49:28 +0600

    message

--- Minimum Size Subarray Sum.js
+++ Minimum Size Subarray Sum.js
@@ -6,21 +6,21 @@
  * @return {number}
  */
 var minSubArrayLen = function(target, nums) {
-    let l = 0, sum = 0;
-    let res = Number.MAX_SAFE_INTEGER;
+  let l = 0, sum = 0;
+  let res = Number.MAX_SAFE_INTEGER;
 
-    for(let r = 0; r < nums.length; r++) {
-        sum += nums[r];
+  for (let r = 0; r < nums.length; r++) {
+    sum += nums[r];
 
-        while (sum >= target) {
-            res = Math.min(res, r-l+1);
-            sum -= nums[l];
-            l++;
-        }
+    while (sum >= target) {
+      res = Math.min(res, r - l + 1);
+      sum -= nums[l];
+      l++;
     }
+  }
 
-    if(res === Number.MAX_SAFE_INTEGER)
-        return 0;
+  if (res === Number.MAX_SAFE_INTEGER)
+    return 0;
 
-    return res;
+  return res;
 };
--- Remove Nth Node From End of List.js
+++ Remove Nth Node From End of List.js
@@ -7,8 +7,8 @@
  */
 
 function ListNode(val, next) {
-    this.val = (val===undefined? 0 : val);
-    this.next = (next===undefined? null : next);
+  this.val = (val === undefined ? 0 : val);
+  this.next = (next === undefined ? null : next);
 }
 
 /**
@@ -17,25 +17,25 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let curr = head;
-    let size = 0;
-    while(curr) {
-        size++;
-        curr = curr.next;
-    }
+  let curr = head;
+  let size = 0;
+  while (curr) {
+    size++;
+    curr = curr.next;
+  }
 
-    if((n===1 && size ===1) || (size < n)) {
-        return null;
-    }
-    if((size - n) === 0)
-        return head.next;
+  if ((n === 1 && size === 1) || (size < n)) {
+    return null;
+  }
+  if ((size - n) === 0)
+    return head.next;
 
-    curr = head;
-    for(let i = 1; i <= (size - n - 1); i++) {
-        curr = curr.next;
-    }
-    if(curr.next)
-        curr.next = curr.next.next;
+  curr = head;
+  for (let i = 1; i <= (size - n - 1); i++) {
+    curr = curr.next;
+  }
+  if (curr.next)
+    curr.next = curr.next.next;
 
-    return head;
+  return head;
 };
